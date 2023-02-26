d0041e84dad24a718b69b24847c93c316208da67
NeelPoriya1309 <202001139@daiict.ac.in>
2023-02-27 00:56:52 +0530

    dep

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,10 +4,10 @@ import nc from "next-connect";
 const handler = nc({
   onError: (err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).end("Something broke!");
+    res.status(500).json({ err: "Something broke!" });
   },
   onNoMatch: (req, res) => {
-    res.status(404).end("Page is not found");
+    res.status(404).json({ err: "Page is not found" });
   },
 });
 
