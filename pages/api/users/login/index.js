eaebf5c2ff988761da9da59cfbe207b5f66bf7bb
NeelPoriya1309 <202001139@daiict.ac.in>
2023-03-04 13:25:11 +0530

    login api

---
+++ pages/api/users/login/index.js
@@ -0,0 +1,28 @@
+const nc = require('next-connect');
+const dbConnect = require('./../../../../lib/mongoose');
+const User = require('./../../../../models/userModel');
+const authController = require('./../../../../controllers/authController');
+
+const handler = nc();
+handler.post(async (req, res) => {
+  await dbConnect();
+
+  const { email, password } = req.body;
+
+  //1) Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+
+  //2) Check if the email is valid or not
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password!', 401));
+  }
+
+  //3) If everything is okay, send the token
+  authController.createSendToken(user, 200, res);
+});
+
+export default handler;
