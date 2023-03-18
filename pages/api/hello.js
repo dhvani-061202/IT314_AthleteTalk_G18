223d19ec841ba612bce57d612fd9f75295aa9a13
NeelPoriya1309 <202001139@daiict.ac.in>
2023-03-18 19:29:09 +0530

    back to make it work online🥲

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,14 +1,8 @@
 const nc = require('next-connect');
-const dbConnect = require('./../../lib/mongoose');
-const Video = require('./../../models/videoModel');
 
 const handler = nc();
 handler.get(async (req, res) => {
-  await dbConnect();
-
-  const videos = await Video.find({});
-
-  res.status(200).json({ videos });
+  res.status(200).json({ name: 'John Doe' });
 });
 
 export default handler;
