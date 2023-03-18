805dbf135205058340bd85d181f9294153c0323f
NeelPoriya1309 <202001139@daiict.ac.in>
2023-03-18 19:33:25 +0530

    last try

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,5 @@
-const nc = require('next-connect');
-
-const handler = nc();
-handler.get(async (req, res) => {
+const handler = (req, res) => {
   res.status(200).json({ name: 'John Doe' });
-});
+};
 
 export default handler;
