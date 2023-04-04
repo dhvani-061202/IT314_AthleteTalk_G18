df446665fe7b04e1529714bcd62e9866e1e92016
Chirag Chavda <202001164@daiict.ac.in>
2023-04-04 16:31:20 +0530

    Added user schema definition for Mongoose with properties for name, email, password, role, and related fields with validation rules.

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
-// const crypto = require('crypto');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -47,65 +47,3 @@ const userSchema = new mongoose.Schema({
     type: Date,
   },
 });
-
-// userSchema.pre('save', async function (next) {
-//   if (!this.isModified('password')) return next();
-
-//   this.password = await bcrypt.hash(this.password, 12);
-//   this.passwordConfirm = undefined;
-
-//   next();
-// });
-
-// userSchema.pre('save', async function (next) {
-//   if (!this.isModified('password') || this.isNew) return next();
-
-//   this.passwordChangedAt = Date.now() - 1000;
-//   next();
-// });
-
-// userSchema.pre(/^find/, function (next) {
-//   // this points to the current query
-//   this.find({ active: { $ne: false } });
-//   next();
-// });
-
-// userSchema.methods.correctPassword = async function (
-//   candidatePassword,
-//   userPassword
-// ) {
-//   return await bcrypt.compare(candidatePassword, userPassword);
-// };
-
-// userSchema.methods.changedPasswordAfter = function (JWTTimeStamp) {
-//   if (this.passwordChangedAt) {
-//     const changedTimeStamp = parseInt(
-//       this.passwordChangedAt.getTime() / 1000,
-//       10
-//     );
-//     return JWTTimeStamp < changedTimeStamp;
-//   }
-//   return false;
-// };
-
-// userSchema.methods.createPasswordResetToken = function () {
-//   const resetToken = crypto.randomBytes(32).toString('hex');
-
-//   this.passwordResetToken = crypto
-//     .createHash('sha256')
-//     .update(resetToken)
-//     .digest('hex');
-
-//   this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
-
-//   return resetToken;
-// };
-
-// let User;
-// try {
-//   User = mongoose.model('User');
-// } catch (err) {
-//   User = mongoose.model('User', userSchema);
-// }
-// // const User = mongoose.model('User') || mongoose.model('User', userSchema);
-// module.exports = User;
