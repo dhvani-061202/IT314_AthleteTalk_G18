e614c051df40568537a5d6acc4d6d42400d70d61
unknown <202001174@daiict.ac.in>
2023-04-04 20:21:37 +0530

    Adding function for server-side rendering in upload videos

--- pages/video/upload/index.js
+++ pages/video/upload/index.js
@@ -3,21 +3,27 @@ import { Box, Button, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import FormDialog from '../../../components/FormDialog';
 import MultipleSelectChip from '../../../components/MultiSelect';
+import server from '../../../server';
+
 
 function UploadVideo({ categories }) {
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [selectedCategories, setSelectedCategories] = useState([]);
-    const extractedCategories = categories.map((category) => category.name);
-    const [fileName, setFileName] = useState('');
-    const [submitLoader, setSubmitLoader] = useState(false);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [file, setFile] = useState(null);
+  const [newCategoryButtonClicked, setNewCategoryButtonClicked] = useState(false);
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  let categoryIds = [];
+
+  const extractedCategories = categories.map((category) => category.name);
+  const [fileName, setFileName] = useState('');
+  const [submitLoader, setSubmitLoader] = useState(false);
 
 
     const handleFileChange = (e) => {
       const file = {
         preview: URL.createObjectURL(e.target.files[0]),
         data: e.target.files[0],
-      };
+      };git 
       setFileName(e.target.value);
       setFile(file);
     };
@@ -97,6 +103,43 @@ function UploadVideo({ categories }) {
         </>
       );
     }
-
     
     export default UploadVideo;
+
+    export const getServerSideProps = async (context) => {
+      const { req, res } = context;
+      if (!req.cookies.jwt) {
+        console.log('Cookie not found🍪🍪');
+        return {
+          redirect: {
+            destination: '/login',
+            permanent: false,
+          },
+        };
+      }
+      try {
+        const categoriesResponse = await fetch(`${server}/api/category`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${req.cookies.jwt}`,
+          },
+        });
+    
+        let categories;
+        if (categoriesResponse.ok) {
+          const data = await categoriesResponse.json();
+          if (!data.data.categories) throw new Error('No categories found');
+          categories = data.data.categories;
+        } else {
+          // console.log(categoriesResponse);
+          throw new Error('Something went wrong!🥲');
+        }
+    
+        return {
+          props: {
+            categories,
+          },
+        };
+      } 
+    };
