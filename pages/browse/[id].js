b3029f1bdeab65a25001de2c954e26d047705c7f
Dhvani Upadhyay <104089036+dhvani-061202@users.noreply.github.com>
2023-04-04 16:01:28 +0530

    Update [id].js
    adding few features to plan detail functionality

--- pages/browse/[id].js
+++ pages/browse/[id].js
@@ -14,3 +14,38 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import { useState } from 'react';
 const { server } = require('./../../../utils/server');
+
+const PlanDetails = ({ plan, planVideos, taken }) => {
+  const router = useRouter();
+  const theme = useTheme();
+  const [planTaken, setPlanTaken] = useState(taken);
+
+  const handleBack = (e) => {
+    e.preventDefault();
+    router.back();
+  };
+
+  const addPlanHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`${server}/api/user-plans`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+        body: JSON.stringify({
+          plan: plan._id,
+        }),
+      });
+      if (response.ok) {
+        setPlanTaken(true);
+      } else {
+        throw new Error('Something went wrong!: ', err);
+      }
+    } catch (err) {
+      console.log(err);
+      alert('err');
+    }
+  };
+};
