5fd2a5363d3f0a863d94947db3b45df8a80ce863
harshacharya15 <202001131@daiict.ac.in>
2023-04-04 23:15:40 +0530

    added delete and upload functionality functions

--- pages/api/videos/upload.js
+++ pages/api/videos/upload.js
@@ -43,3 +43,34 @@ const authenticateGoogle = () => {
   });
   return auth;
 };
+
+const delteFile = (path) => {
+  fs.unlink(path, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+};
+
+const uploadToGoogleDrive = async (file, auth) => {
+  const fileMetadata = {
+    name: file.originalname,
+    parents: ['1boTQtsxeCHDdudRHDF_z-_GA2aqywwcp'],
+  };
+
+  const media = {
+    mimeType: file.mimetype,
+    body: fs.createReadStream(file.path),
+  };
+
+  const driveService = google.drive({ version: 'v3', auth });
+
+  const response = await driveService.files.create({
+    requestBody: fileMetadata,
+    media: media,
+  });
+
+  delteFile(file.path);
+  return response;
+};
