7394d7a8fb75f05fb3f384a4abca82e4da95707b
Dhvani Upadhyay <104089036+dhvani-061202@users.noreply.github.com>
2023-04-04 17:14:38 +0530

    Adding returning object to the .js file

--- pages/browse/[id].js
+++ pages/browse/[id].js
@@ -48,4 +48,82 @@ const PlanDetails = ({ plan, planVideos, taken }) => {
       alert('err');
     }
   };
+  
+  return (
+    <>
+      <Button onClick={handleBack} variant="contained">
+        Back
+      </Button>
+      <Box>
+        <Typography variant="h4">{plan.name}</Typography>
+        <Typography variant="h6">{plan.description}</Typography>
+        <Typography variant="h6">
+          <b>Created By:</b> {plan.creator.name}
+        </Typography>
+        <Typography variant="h6" sx={{ mb: 1, display: 'inline' }}>
+          Categories:{' '}
+        </Typography>
+        {plan.categories.map((category, idx) => {
+          return (
+            <Typography
+              sx={{
+                border: `2px solid ${theme.palette.primary.main}`,
+                color: 'white',
+                width: 'fit-content',
+                padding: 0.76,
+                mr: 0.3,
+                borderRadius: '20px',
+                backgroundColor: theme.palette.primary.main,
+              }}
+              key={idx}
+              variant="p"
+            >
+              {category.name}
+            </Typography>
+          );
+        })}
+
+        {planVideos.map((videoDay, idx) => {
+          return (
+            <Box key={idx}>
+              <Typography variant="h5" sx={{ mt: 2 }}>
+                {' '}
+                Day {idx + 1}
+              </Typography>
+              <TableContainer component={Paper}>
+                <Table sx={{}} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell component={'th'}>No. </TableCell>
+                      <TableCell component={'th'}>Video Name</TableCell>
+                    </TableRow>
+                    {videoDay.map((video, idx) => {
+                      return (
+                        <TableRow key={idx}>
+                          <TableCell>{idx + 1}</TableCell>
+                          <TableCell sx={{ width: '80%' }}>
+                            {video.title}
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })}
+                  </TableHead>
+                </Table>
+              </TableContainer>
+            </Box>
+          );
+        })}
+      </Box>
+      {!planTaken && (
+        <Button onClick={addPlanHandler} variant="contained">
+          Take
+        </Button>
+      )}
+      {planTaken && (
+        <Button disabled variant="contained">
+          Already Taken!
+        </Button>
+      )}
+    </>
+  );
 };
