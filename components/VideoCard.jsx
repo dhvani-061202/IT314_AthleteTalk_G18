20e488bee8fa7e55bf2fb2a7a55519888e4eef63
NeelPoriya1309 <202001139@daiict.ac.in>
2023-04-05 20:38:14 +0530

    Added Video Card Component

---
+++ components/VideoCard.jsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+export default function VideoCard({ details }) {
+  return (
+    <Card sx={{ width: "20vw", height: "55vh" }}>
+      <iframe
+        src={`https://drive.google.com/file/d/${details.gDriveID}/preview`}
+        style={{ minHeight: 230, width: "100%" }}
+        allow="autoplay"
+        allowFullScreen
+      ></iframe>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {details.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {details.description}
+        </Typography>
+      </CardContent>
+      <CardContent>
+        <Typography variant="body2" color="primary">
+          Categories
+        </Typography>
+        {details.categories.map((category, idx) => {
+          if (idx < 3)
+            return (
+              <Typography
+                key={idx}
+                sx={{
+                  ml: 2,
+                  mt: 0.5,
+                  borderRadius: 5,
+                  bgcolor: "primary.light",
+                  width: "fit-content",
+                  pl: 1,
+                  pr: 1,
+                }}
+                variant="body2"
+                color="white"
+              >
+                {category.name}
+              </Typography>
+            );
+        })}
+      </CardContent>
+    </Card>
+  );
+}
