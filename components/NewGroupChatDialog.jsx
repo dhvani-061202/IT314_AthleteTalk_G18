26d3a6357a36772266dee28cea6e647108c84f5b
harshacharya15 <202001131@daiict.ac.in>
2023-04-25 21:55:00 +0530

    added front end code for new group chat

--- components/NewGroupChatDialog.jsx
+++ components/NewGroupChatDialog.jsx
@@ -98,4 +98,123 @@ export default function NewGroupChatDialog({ setAllChats }) {
 
     handleClose();
   };
+  
+  const handleSearch = async (query) => {
+    // search for users
+    setSearch(query);
+    if (query.length === 0) {
+      setTimeout(() => {
+        setQueriedUsers([]);
+      }, 1000);
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const queriedUsers = await fetch(`/api/users/find?search=${query}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: 'Bearer ' + localStorage.getItem('token'),
+        },
+      });
+      const queriedUsersData = await queriedUsers.json();
+
+      setQueriedUsers(queriedUsersData.users);
+    } catch (error) {
+      setOpenSnackbar(true);
+      setSnackbarMessage('Fetching users failed');
+      setSnackbarSeverity('error');
+
+      // alert(error);
+    }
+
+    setLoading(false);
+  };
+
+  const [openSnackbar, setOpenSnackbar] = React.useState(false);
+  const [snackbarMessage, setSnackbarMessage] = React.useState('');
+  const [snackbarSeverity, setSnackbarSeverity] = React.useState('success');
+
+  const onClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpenSnackbar(false);
+  };
+
+  return (
+    <div>
+      <Button
+        variant="standard"
+        sx={{
+          display: 'inline-block',
+          textTransform: 'none',
+          bgcolor: '#74b9ff',
+        }}
+        onClick={handleClickOpen}
+      >
+        New Group Chat
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        TransitionComponent={Transition}
+        keepMounted
+        fullWidth
+      >
+        <Typography p={3} paddingTop={2} paddingBottom={0} variant="h5">
+          Create Group Chat
+        </Typography>
+        <DialogContent>
+          <FormControl fullWidth>
+            <TextField
+              id="outlined-basic"
+              label="Chat Name"
+              variant="outlined"
+              inputRef={chatName}
+            ></TextField>
+            <br></br>
+            <TextField
+              id="outlined-basic"
+              label="Add Users"
+              variant="outlined"
+              onChange={(e) => handleSearch(e.target.value)}
+            ></TextField>
+            {/* Show selected Users */}
+            <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+              {selectedUsers.map((user) => {
+                return (
+                  <Box
+                    key={user.name}
+                    sx={{
+                      display: 'flex',
+                      backgroundColor: '#e5d1ff',
+                      padding: '5px',
+                      margin: '5px',
+                      borderRadius: '5px',
+                      marginTop: '5px',
+                      width: 'fit-content',
+                    }}
+                  >
+                    <Avatar
+                      sx={{ width: '25px', height: '25px' }}
+                      src={`https://api.dicebear.com/6.x/micah/svg?seed=${user.name}+`}
+                    />
+                    <Typography variant="body1">{user.name}</Typography>
+                    <CloseIcon
+                      onClick={(e) => {
+                        setSelectedUsers(
+                          selectedUsers.filter((u) => u.name !== user.name)
+                        );
+                      }}
+                    />
+                  </Box>
+                );
+              })}
+            </Box>
 }
