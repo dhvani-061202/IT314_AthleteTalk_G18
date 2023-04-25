006a4b7ea16510a045e5a8998bc27cbfe8011586
202001160 <202001160@daiict.ac.in>
2023-04-25 21:53:23 +0530

    added fronted for scorrableChat

--- components/ScorrableChat.jsx
+++ components/ScorrableChat.jsx
@@ -55,3 +55,86 @@ const getSender = (loggedUser, users) => {
 const getSenderFull = (loggedUser, users) => {
   return users[0]._id === loggedUser._id ? users[1] : users[0];
 };
+const ScrollableChat = ({ messages }) => {
+  console.log(messages);
+  //   useEffect(() => {
+  //     socketInitializer();
+
+  //     return () => {
+  //       if (socket) {
+  //         socket.disconnect();
+  //       }
+  //     };
+  //   }, []);
+  const authContext = useContext(AuthContext);
+  const user = authContext.user;
+  //   const [socketConnected, setSocketConnected] = useState(false);
+
+  //   async function socketInitializer() {
+  //     console.log('creating socket connection');
+  //     await fetch('/api/socket');
+
+  //     socket = io();
+  //     socket.emit('setup', user);
+  //   }
+
+  return (
+    <ScrollableFeed>
+      {messages &&
+        messages.map((m, i) => {
+          return (
+            <Box sx={{ display: 'flex' }} key={m._id}>
+              {(isSameSender(messages, m, i, authContext.user._id) ||
+                isLastMessage(messages, i, authContext.user._id)) && (
+                <Tooltip title={m.sender.name} arrow>
+                  <Avatar
+                    sx={styles.avatar}
+                    src={
+                      m.sender.imageUrl
+                        ? m.sender.imageUrl
+                        : `https://api.dicebear.com/6.x/micah/svg?seed=${m.sender?.name}+`
+                    }
+                  />
+                </Tooltip>
+              )}
+              <span
+                style={{
+                  backgroundColor: `${
+                    m.sender._id === user._id ? '#bee3f8' : '#b9f5d0'
+                  }`,
+                  marginLeft: isSameSenderMargin(messages, m, i, user._id),
+                  marginTop: isSameUser(messages, m, i, user._id) ? 3 : 10,
+                  borderRadius: '20px',
+                  padding: '5px 15px',
+                  maxWidth: '75%',
+                }}
+              >
+                {m.content}
+              </span>
+            </Box>
+          );
+        })}
+      <br></br>
+    </ScrollableFeed>
+  );
+};
+
+/** @type {import("@mui/material").SxProps} */
+const styles = {
+  avatorContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    my: 5,
+    flexDirection: 'column',
+  },
+  avatar: {
+    width: '40px',
+    height: '40px',
+    // borderRadius: 0,
+  },
+  yourChannel: {
+    mt: 1,
+  },
+};
+
+export default ScrollableChat;
