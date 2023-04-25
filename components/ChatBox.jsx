8940450ef73f72dfb97ae1fa4df37fb07e074271
AS-171 <202001171@daiict.ac.in>
2023-04-25 21:32:57 +0530

    initializing socket in chatbox

--- components/ChatBox.jsx
+++ components/ChatBox.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
-import { useState } from 'react';
+import {
+  Box,
+  FilledInput,
+  FormControl,
+  Paper,
+  TextField,
+  Typography,
+} from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import ScrollableChat from './ScrollableChat';
+
+import { io } from 'socket.io-client';
+import { useContext } from 'react';
+import AuthContext from '../store/auth-context';
+import server from '../server';
+
+const ENDPOINT = `${server}/api/socket`;
+var socket = io(),
+  selectedChatCompare;
 
 const ChatBox = () => {
+
+  const authContext = useContext(AuthContext);
+  const user = authContext.user;
+  useEffect(() => {
+    socketInitializer();
+
+    return () => {
+      if (socket) {
+        socket.disconnect();
+      }
+    };
+  }, [user]);
+  
   const [socketConnected, setSocketConnected] = useState(false);
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(false);
